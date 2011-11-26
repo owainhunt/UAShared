2f49217164a651b345c6d4d5ff058d8951dd1b47
Owain R Hunt <owainhunt@btinternet.com>
2011-11-26 01:35:53 +0000

    Add NSManagedObject category.

---
+++ NSManagedObject+Utilities.h
@@ -0,0 +1,15 @@
+//
+//  NSManagedObject+Utilities.h
+//  
+//
+//  Created by Owain Hunt on 26/11/2011.
+//  Copyright (c) 2011 Owain R Hunt. All rights reserved.
+//
+
+
+
+@interface NSManagedObject (Utilities)
+
+- (id)managedObjectWithEntityName:(NSString *)entityName fromDictionary:(NSDictionary *)dictionary;
+
+@end
---
+++ NSManagedObject+Utilities.m
@@ -0,0 +1,39 @@
+//
+//  NSManagedObject+Utilities.m
+//  
+//
+//  Created by Owain Hunt on 26/11/2011.
+//  Copyright (c) 2011 Owain R Hunt. All rights reserved.
+//
+
+#import "NSManagedObject+Utilities.h"
+
+@implementation NSManagedObject (Utilities)
+
+- (id)managedObjectWithEntityName:(NSString *)entityName fromDictionary:(NSDictionary *)dictionary
+{
+    NSFetchRequest *request = [[NSFetchRequest alloc] init];
+    [request setEntity:[NSEntityDescription entityForName:entityName inManagedObjectContext:self]];
+    
+    id managedObject;
+    
+    if ([self countForFetchRequest:request error:nil] == 0)
+    {
+        managedObject = [NSClassFromString(entityName) insertInManagedObjectContext:self];
+        for (NSString *key in [NSObject propertyKeysForClass:NSClassFromString(entityName)])
+        {
+            if ([[dictionary allKeys] containsObject:[key toUnderscore]])
+            {
+                [managedObject setValue:[dictionary objectForKey:[key toUnderscore]] forKey:key];
+            }
+        }
+    }
+    else
+    {
+        managedObject = [[self executeFetchRequest:request error:nil] objectAtIndex:0];
+    }
+    
+    return managedObject;
+}
+
+@end
