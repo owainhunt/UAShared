a28be9b8a10ea60c2f87610bd693b93570eb9a4c
Owain R Hunt <owainhunt@btinternet.com>
2012-04-02 22:30:42 +0100

    It's a framework now!

--- UAShared/UAShared/UAShared.h
+++ UAShared/UAShared/UAShared.h
@@ -7,6 +7,35 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "NSManagedObjectContext+Fetching.h"
+#import "NSAttributeDescription+ColumnTypes.h"
+#import "NSManagedObject+JSON.h"
+#import "NSManagedObject+URI.h"
+#import "NSManagedObjectContext+Utilities.h"
+#import "NSData+Base64.h"
+#import "NSObject+Utilities.h"
+#import "NSDate+Utilities.h"
+#import "NSString+Utilities.h"
+#import "NSArray+Utilities.h"
+
+#ifdef DEBUG
+#    define UALog(fmt, ...) NSLog(@"%s:%d (%s): " fmt, __FILE__, __LINE__, __func__, ## __VA_ARGS__)
+#else
+#    define UALog(...) /* */
+#endif
+
+// https://github.com/Machx/Zangetsu/blob/master/Source/CWMacros.h
+
+#define UAIBOutletAssert(_x_) \
+do { \
+if(_x_ == nil) { \
+UALog(@"IBOutlet Assertion: %s is nil and appears to not be hooked up!",#_x_); \
+} \
+} while(0);
+
+#define unless(x) if(!x)
+#define until(x) while(!x)
+
 
 @interface UAShared : NSObject
 
